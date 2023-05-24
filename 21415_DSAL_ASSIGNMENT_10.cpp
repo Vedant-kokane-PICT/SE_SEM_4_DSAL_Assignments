aab6ef390edf1f3f40821c723ac5c8e39a0be6ef
Vedant Kokane (PICT) duplicate <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2023-05-24 18:44:08 +0530

    Update 21415_DSAL_ASSIGNMENT_10.cpp

--- 21415_DSAL_ASSIGNMENT_10.cpp
+++ 21415_DSAL_ASSIGNMENT_10.cpp
@@ -11,116 +11,125 @@
 using namespace std;
 
 class Heap{
-	int *array;
-	int total;
-public:
-	Heap(int n){
-		total = n;
-		array = new int[total];
-		for(int i=0;i<total;i++){
-			array[i] = 0;
-		}
-	}
-
-	void create(){
-		for(int i=0;i<total;i++){
-			cout<<"Enter the marks - "<<endl;
-			cin>>array[i];
-		}
-	}
-
-	void swap(int &a,int &b){
-		int temp = a;
-		a = b;
-		b = temp;
-	}
-
-	void heapify(int i,int n){
-		// Finding largest among root, left child, and right child
-	    int largest = i;
-	    int left = 2 * i + 1;
-	    int right = 2 * i + 2;
-	  
-	    if (left < n && array[left] > array[largest])
-	      largest = left;
-	  
-	    if (right < n && array[right] > array[largest])
-	      largest = right;
-	  
-	    // Swap and continue heapifying if root is not largest
-	    if (largest != i) {
-	      swap(array[i], array[largest]);
-	      heapify(largest,n);
-	    }
-	}
-
-	void heapSort(){
-	    for (int i = total / 2 - 1; i >= 0; i--){
-	      heapify(i,total);
-	    }
-	  
-	    // Heap sort
-	    for (int i = total - 1; i >= 0; i--) {
-	      swap(array[0], array[i]);
-	  
-	     // Heapify root element to get highest element at root again
-	     heapify(0,i);
-	    }
-	}
-
-	void display(){
-		for(int i=0;i<total;i++){
-			cout<<array[i]<<" ";
-		}
-		cout<<endl;
-	}
-
-	void highest(){
+  int arr[100] = {0};
+  int index;
+  public:
+    Heap(){
+        index = 1;
+    }
+    
+    void createRecord(){
+        int n;
+        cout<<"Enter total records"<<endl;
+        cin>>n;
+        for(int i=0;i<n;i++){
+            cout<<"Enter marks - "<<endl;
+            cin>>arr[index];
+            index++;
+        }
+    }
+    
+    void printRecords(){
+        for(int i=1;i<index;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    void insert(int n){
+        int i = n;
+        int temp = arr[n];
+        while(i>1 && arr[i/2] < temp){
+            arr[i] = arr[i/2];
+            i = i/2;
+        }
+        arr[i] = temp;
+    }
+    
+    void delete1(int n){
+        int i = 1;
+        int j = 2*i;
+        swap(arr[1],arr[n]);
+        while(j <= n-1){
+            if(j < n-1 && (arr[j+1] > arr[j])){
+                j = j+1; 
+            }
+            
+            if(arr[j] > arr[i]){
+                swap(arr[j],arr[i]);
+                i = j;
+                j = 2*j;
+            }
+            else{
+                break;
+            }
+            
+        }
+    }
+    
+    void heapify(){
+        for(int i = 2;i<=index;i++){
+            insert(i);
+        }
+    }
+    
+    void heapSort(){
+        heapify();
+        for(int i=index-1;i>1;i--){
+            delete1(i);
+        }
+    }
+    
+    void swap(int &a,int &b){
+        int temp = a;
+        a = b;
+        b =temp;
+    }
+    
+    void lowest(){
 		heapSort();
-		cout<<"Hightest Marks - "<<array[total-1]<<endl;
-	}
-
-	void lowest(){
-		heapSort();
-		cout<<"Lowest Marks - "<<array[0]<<endl;
+		cout<<"Lowest Marks - "<<arr[1]<<endl;
 	}
 };
-// use 7 nodes in tree because it is a completed tree
 
-int main() {
-	cout<<"Enter total no of entries - "<<endl;
-	int n;
-	cin>>n;
-	Heap h1(n);
-	char ch = 'y';
+// use examples of 7 nodes because a tree with 7 nodes is a complete binary tree
+int main()
+{
+    Heap h1;
+    char ch = 'y';
 	bool heapApplied = 0;
 	while(ch != 'n'){
 		cout<<"-----------------------------"<<endl;
 		cout<<"1. Add Marks of students"<<endl;
 		cout<<"2. Display all Marks"<<endl;
-		cout<<"3. Apply Heap Sort"<<endl;
-		cout<<"4. Display the lowest marks"<<endl;
+		cout<<"3. Heapify the record"<<endl;
+		cout<<"4. Apply Heap Sort"<<endl;
+		cout<<"5. Display the lowest marks"<<endl;
 		cout<<"-----------------------------"<<endl;
 		int c;
 		cout<<"Enter choice - "<<endl;
 		cin>>c;
 		if(c==1){
-			h1.create();
+			h1.createRecord();
 		}
 		else if(c==2){
 			if(heapApplied){
 				cout<<"Heap sort has been applied."<<endl;
-				h1.display();
+				h1.printRecords();
 			}
 			else{
-				h1.display();
+				h1.printRecords();
 			}
 		}
 		else if(c==3){
-			h1.heapSort();
-			cout<<"Heap sort is  applied."<<endl;
+			h1.heapify();
+			cout<<"The record is heapified"<<endl;
 		}
 		else if(c==4){
+		    h1.heapSort();
+			cout<<"Heap sort is applied."<<endl;
+		}
+		else if(c==5){
 			h1.lowest();
 		}
 		else{
