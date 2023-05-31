250bd9b051d4ab209b9c04dfe611cdc41c995916
Vedant Kokane (PICT) duplicate <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2023-05-31 10:15:58 +0530

    Update 21415_DSAL_ASSIGNMENT_7.cpp

--- 21415_DSAL_ASSIGNMENT_7.cpp
+++ 21415_DSAL_ASSIGNMENT_7.cpp
@@ -6,138 +6,123 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
+#include<iostream>
 #include<string.h>
 using namespace std;
-#define m 10
-
-
 
 class Graph{
-    int **graph;
+	int **graph;
+	char vertex[26];
 	int vertices,edges;
 public:
+	Graph(int v1,int e1){
+		vertices = v1;
+		edges = e1;
+		graph = new int*[vertices];
+		for(int i=0;i<vertices;i++){
+			graph[i] = new int[vertices];
+		}
 
-    Graph(int v,int e){
-        vertices = v;
-        edges = e;
-        graph = new int*[vertices];
-        for (int i = 0; i < vertices; i++) {
-            graph[i] = new int[vertices];
-        }
-        
-        for (int i = 0; i < vertices; i++) {
-            for (int j = 0; j < vertices; j++) {
-                // Assign values to the
-                // memory blocks created
-                graph[i][j] = 0;
-            }
-        }
-    }
-    
-	void createGraph(){
-		int v = vertices;
+		for(int i=0;i<vertices;i++){
+			for(int j=0;j<vertices;j++){
+				graph[i][j] = 0;
+			}
+		}
+		int x =0;
+		for (char i = 'a'; i <= 'z'; i++) {
+	      	vertex[x] = i;
+	      	x++;
+	  	}
+	}
+
+	void create(){
+		cout<<"Vertices are - ";
+		for(int i=0;i<vertices;i++){
+			cout<<vertex[i]<<" ";
+		}
+		cout<<endl;
 		for(int i=0;i<edges;i++){
-			int a,b,w;
-			cout<<"Enter the the 2 vertices of edge "<<endl;
-			cin>>a>>b;
-			cout<<"Enter the weight of edge"<<endl;
-			cin>>w;
-			graph[a][b] = w;
-			graph[b][a] = w;
+			cout<<"Enter 2 vertices of edge and weight of the edge"<<endl;
+			char a,b;
+			int c;
+			cin>>a>>b>>c;
+			int u = returnInt(a);
+			int v = returnInt(b);
+			graph[u][v] = c;
+			graph[v][u] = c;
 		}
 	}
 
-	void display(){
-		int i, j;
-    	cout<<"\n\n"<<"\t"<<"";
-    	for(i = 0; i < vertices; i++)
-    		cout<<"\t"<<"("<<i<<")";
-    	cout<<"\n\n";
-    	for(i = 0; i < vertices; i++)
-    	{
-    		cout<<"\t"<<"("<<i<<")";
-    		for(j = 0; j < vertices; j++)
-    		{
-    			cout<<"\t"<<graph[i][j];
-    		}
-    		cout<<"\n\n";
-    	}
+	int returnInt(char x){
+		for(int i=0;i<vertices;i++){
+			if(x == vertex[i]){
+				return i;
+			}
+		}
+		return -1;
 	}
 
-	// Function to find the vertex with minimum key value 
-    int minKey(int key[], bool visited[])  
-    { 
-        int min = 999, minIndex;  // 999 represents an Infinite value
-    
-        for (int v = 0; v < vertices; v++) { 
-            if (visited[v] == false && key[v] < min) { 
-            	// vertex should not be visited
-                min = key[v];
-    			minIndex = v;  
-            }
-        }    
-        return minIndex;  
-    } 
-    
-    // Function to print the final MST stored in parent[]  
-    void printMST(int parent[])  
-    {  
-        int minCost=0;
-    	cout<<"Edge \tWeight\n";  
-        for (int i = 1; i< vertices; i++) {
-    		cout<<parent[i]<<" - "<<i<<" \t"<<graph[i][parent[i]]<<" \n";  
-    		minCost+=graph[i][parent[i]];
-        }
-    	cout<<"Total cost is - "<<minCost;
-    }  
-    
-    // Function to find the MST using adjacency graph matrix representation  
-    void find_MST()  
-    {  
-        int parent[vertices], key[vertices];
-        bool visited[vertices];
-    
-        // Initialize all the arrays 
-        for (int i = 0; i< vertices; i++) { 
-            key[i] = 999;    // 999 represents an Infinite value
-            visited[i] = false;
-            parent[i]=-1;
-        }    
-    
-        key[0] = 0;  // Include first vertex in MST by setting its key vaue to 0.  
-        parent[0] = -1; // First node is always root of MST  
-    
-        // The MST will have maximum V-1 vertices  
-        for (int x = 0; x < vertices - 1; x++) 
-        {  
-            // Finding the minimum key vertex from the 
-            //set of vertices not yet included in MST  
-            int u = minKey(key, visited);  
-    
-            visited[u] = true;  // Add the minimum key vertex to the MST  
-    
-            // Update key and parent arrays
-            for (int v = 0; v < vertices; v++)  
-            {
-                // graph[u][v] is non zero only for adjacent vertices of u  
-                // visited[v] is false for vertices not yet included in MST  
-                // key[] gets updated only if cost[u][v] is smaller than key[v]  
-                if (graph[u][v]!=0 && visited[v] == false && graph[u][v] < key[v])
-                {  
-                    parent[v] = u;
-                    key[v] = graph[u][v];  
-                }        
-            }
-        }
-    
-        // print the final MST  
-    	printMST(parent);  
-    }  
+	void display(){
+		for(int i=0;i<vertices;i++){
+			cout<<"\t"<<vertex[i]<<"\t";
+		}
+		cout<<endl;
+		for(int i=0;i<vertices;i++){
+			cout<<vertex[i]<<"\t";
+			for(int j=0;j<vertices;j++){
+				cout<<graph[i][j]<<"\t\t";
+			}
+			cout<<endl;
+		}
+	}
+
+	int minKey(bool visited[],int key[]){
+		int min = 999,minIndex;
+		for(int i=0;i<vertices;i++){
+			if(visited[i] == 0 && key[i] < min){
+				min = key[i];
+				minIndex = i;
+			}
+		}
+		return minIndex;
+	}
+
+	void Prim(){
+		int key[vertices],parent[vertices];
+		bool visited[vertices];
+		for(int i=0;i<vertices;i++){
+			key[i] = 999;
+			visited[i] = 0;
+			parent[i] = -1;
+		}
+		key[0] = 0;
+		for(int i=0;i<vertices-1;i++){
+			int u = minKey(visited,key);
+			visited[u] = 1;
+			for(int v=0;v<vertices;v++){
+				if((graph[u][v] != 0 && visited[v] == 0) && (graph[u][v] < key[v])){
+					parent[v] = u;
+					key[v] = graph[u][v];
+				}
+			}
+		}
+		printMST(parent);
+	}
+
+	void printMST(int parent[]){
+		int cost = 0;
+		cout<<endl;
+		cout<<"Edges "<<"\t"<<"Cost"<<endl;
+		for(int i=1;i<vertices;i++){
+			cout<<vertex[parent[i]]<<" - "<<vertex[i]<<"\t "<<graph[i][parent[i]]<<endl;
+			cost+=graph[i][parent[i]];	
+		}
+		cout<<"Total cost - "<<cost<<endl;
+	}
+
 };
 
-
-int main() {
+int main(){
 	int v,e;
 	cout<<"Enter total no of vertices and total no of edges"<<endl;
 	cin>>v>>e;
@@ -153,13 +138,13 @@ int main() {
 	    cout<<"Enter your choice"<<endl;
 	    cin>>ch;
 	    if(ch == 1){
-	        g1.createGraph();
+	        g1.create();
 	    }
 	    else if(ch == 2){
 	        g1.display();
 	    }
 	    else if(ch == 3){
-	        g1.find_MST();
+	        g1.Prim();
 	        cout<<endl;
 	    }else{
 	        cout<<"You have eneterd a wrong choice"<<endl;
@@ -168,16 +153,22 @@ int main() {
 	    cin>>c;
 	}
 	return 0;
+
 }
 
 /* example
-0,1 - 4
-0,5 - 2
-1,2 - 6
-1,5 - 3
-2,3 - 3 
-2,5 - 1 
-4,3 - 2
+Input - 
+Vertices - 4 , Edges - 5
+a b 10
+a c 11
+c b 17
+c d 11
+d b 6
 
-MST total cost is 11 
+Output - 
+Edges 	Cost
+a - b	 10
+a - c	 11
+b - d	 6
+Total cost - 27
 */
